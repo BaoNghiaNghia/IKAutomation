a96e0cc2c86ec75a2cd35020b68a14e4ddee367f
BaoNghiaNghia <baonghia.kht@gmail.com>
2026-08-09 10:07:48 +0700

    update code

--- ADB/Core/ResourceSearch/ResourceAreaLv2PointSelector.cs
+++ ADB/Core/ResourceSearch/ResourceAreaLv2PointSelector.cs
@@ -7,7 +7,19 @@ namespace ADB_Tool_Automation_Post_FB.Core.ResourceSearch
 {
     public sealed class ResourceAreaLv2PointSelector
     {
-        private static readonly Point[] BasePoints = new[] { new Point(210,261),new Point(178,276),new Point(199,306),new Point(225,279),new Point(302,265),new Point(274,294),new Point(268,327),new Point(279,357),new Point(304,356),new Point(342,339),new Point(323,302),new Point(321,240),new Point(339,267),new Point(348,292),new Point(369,303),new Point(428,258),new Point(461,245),new Point(469,177),new Point(422,201),new Point(525,204),new Point(517,245),new Point(549,267),new Point(567,266),new Point(609,261),new Point(609,218) };
+        private static readonly Point[] BasePoints = new[]
+        {
+            new Point(210,261),new Point(178,276),new Point(199,306),new Point(225,279),new Point(302,265),
+            new Point(274,294),new Point(268,327),new Point(279,357),new Point(304,356),new Point(342,339),
+            new Point(323,302),new Point(321,240),new Point(339,267),new Point(348,292),new Point(369,303),
+            new Point(428,258),new Point(461,245),new Point(469,177),new Point(422,201),new Point(525,204),
+            new Point(517,245),new Point(549,267),new Point(567,266),new Point(609,261),new Point(609,218),
+            new Point(319,702),new Point(330,681),new Point(340,664),new Point(357,654),new Point(370,636),
+            new Point(395,623),new Point(403,634),new Point(407,641),new Point(421,654),new Point(423,669),
+            new Point(408,673),new Point(395,680),new Point(398,693),new Point(380,714),new Point(360,693),
+            new Point(342,699),new Point(324,707),new Point(325,720),new Point(330,734),new Point(348,747),
+            new Point(370,740),new Point(363,714)
+        };
         private static readonly IReadOnlyList<Point> BasePointView = Array.AsReadOnly(BasePoints);
         private readonly Dictionary<string, List<Point>> bags = new Dictionary<string, List<Point>>();
         private readonly Random random;
